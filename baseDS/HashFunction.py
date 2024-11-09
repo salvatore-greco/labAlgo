a05f1b75ac57db2ca30d242eaaf70fcdacf9982b
salvatore-greco <greco.salvatore03@gmail.com>
2024-11-09 18:59:49 +0100

    aggiunto commento sega

--- baseDS/HashFunction.py
+++ baseDS/HashFunction.py
@@ -4,6 +4,6 @@ def h(k,i, m):
     def _h1():
         return k%m
     def _h2():
-        return (2*_h1())+1
-
+        return (2*_h1())+1 #da ricontrollare a ricevimento col professore perchè voglio usare un doppio hash
+    #h2 deve generare sempre un numero dispari in modo che sia sempre primo rispetto a m
     return (_h1()+(i*_h2()))%m
