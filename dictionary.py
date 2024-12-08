9b9e3bba6f49476662123f44eac06a2fd5d3a1ea
salvatore-greco <greco.salvatore03@gmail.com>
2024-12-08 15:38:04 +0100

    FEAT: Aggiunta dimensione dizionario

--- dictionary.py
+++ dictionary.py
@@ -5,18 +5,22 @@ class Dictionary(DataStructure):
 
     def __init__(self, baseDS: DataStructure):
         self._baseDS: DataStructure = baseDS
+        self.size = 0
 
     def insert(self, element: DataStructureElement):
         self._baseDS.insert(element)
+        self.size += 1
 
     def insertKV(self, key, value):
         self._baseDS.insertKV(key, value)
+        self.size += 1
 
     def search(self, target):
         return self._baseDS.search(target)  # gestire l'eccezione
 
     def delete(self, target):
         self._baseDS.delete(target)  # gestire l'eccezione
+        self.size -= 1
 
     def __iter__(self):
         self._iterator = iter(self._baseDS)
