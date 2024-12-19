a855a379cabd5c3e244ae31c0155b031e946ef62
salvatore-greco <greco.salvatore03@gmail.com>
2024-12-19 10:01:24 +0100

    FIX: fixato valore upperbound, calcolato rispetto a size

--- dataGenerator.py
+++ dataGenerator.py
@@ -6,22 +6,23 @@ class DataGenerator:
     def __init__(self):
         self._generator = np.random.default_rng()
 
-    def generateKey(self, size, upper_bound=100000):
+    def generateKey(self, size):
         """
         Genera una sequenza pseudo casuale di interi senza ripetizione.
         :param size: dimensione dell'array di numeri casuali da generare
-        :param upper_bound: dimensione dell'intervallo di generazione
         :return: numpy.ndarray di dimensione size con i numeri generati casualmente
         """
+        upper_bound = size*3
         arr = self._generator.choice(upper_bound, size=size, replace=False)
         self._generator.shuffle(arr)
         return arr
 
-    def generateValue(self, size, upper_bound=100000):
+    def generateValue(self, size):
         """
         Genera una sequenza pseudocasuale di interi, con possibile ripetizione
         :param size: dimensione dell'array di numeri casuali da generare
-        :param upper_bound: dimensione dell'intervallo di generazione
         :return: numpy.ndarray di dimensione size con i numeri generati casualmente
         """
+        upper_bound = size*3
+
         return self._generator.integers(low=0, high=upper_bound, size=size)
